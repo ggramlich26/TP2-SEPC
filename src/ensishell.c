1686d773f84297c89b141672f541840e39a4f39f
Georg Gramlich <georg.gramlich@online.de>
2015-10-16 13:59:45 +0200

    Up to date version

--- src/ensishell.c
+++ src/ensishell.c
@@ -7,9 +7,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <wordexp.h>
+#include <errno.h>
 
 #include "variante.h"
 #include "readcmd.h"
+#include "pidlist.h"
 
 #ifndef VARIANTE
 #error "Variante non défini !!"
@@ -24,6 +30,73 @@
 #if USE_GUILE == 1
 #include <libguile.h>
 
+
+Pid_List pidTable;
+
+pid_t create_processes(char ***processes, char *in, char *out){
+	int pipe_in[2]; 
+	int pipe_out[2];
+	pid_t last_process;
+	for(int i = 0; processes[i] != NULL; i++){
+		if(i >= 2){ 
+			close(pipe_in[0]);
+			close(pipe_in[1]); 
+		}
+		pipe_in[0] = pipe_out[0];
+	   	pipe_in[1] = pipe_out[1];
+		if(processes[i+1] != NULL){
+			pipe(pipe_out);
+		}
+
+//		wordexp_t p;
+//		wordexp(processes[i], &p, 0);
+//		for(int j = 0; j < p.we_wordc; j++){
+//			printf("%s\n", p.we_wordv[j]);
+//		}
+//		wordfree(&p);
+
+		pid_t process = fork();
+		if(process == 0){
+			if(i == 0 && in != NULL){
+				int inDescriptor = open(in, O_RDONLY);
+				dup2(inDescriptor, 0);
+				close(inDescriptor);
+			}
+			if(i!=0){
+				dup2(pipe_in[0], 0);
+				close(pipe_in[0]);
+				close(pipe_in[1]);
+			}
+			if(processes[i+1] != NULL){
+				dup2(pipe_out[1], 1);
+				close(pipe_out[0]);
+				close(pipe_out[1]);
+			}
+			else if(out != NULL){
+				int outDescriptor = open(out, O_WRONLY | O_CREAT);
+				dup2(outDescriptor, 1);
+				close(outDescriptor);
+			}
+			int err = execvp(processes[i][0], processes[i]);
+			if(err == -1){
+				perror(NULL);
+				exit(0);
+			}
+		}
+		else{
+			add_pid_list(&pidTable, process, processes[i][0]);
+			if(processes[i+1]==NULL){
+				last_process = process;
+			}
+		}
+		if(processes[i+1] == NULL && i > 0){
+			close(pipe_in[0]);
+			close(pipe_in[1]);
+		}
+	}
+	return last_process;
+}
+
 int executer(char *line)
 {
 	/* Insert your code to execute the command line
@@ -31,10 +104,23 @@ int executer(char *line)
 	 * parsecmd, then fork+execvp, for a single command.
 	 * pipe and i/o redirection are not required.
 	 */
-	printf("Not implemented: can not execute %s\n", line);
+	//printf("Not implemented: can not execute %s\n", line);
+
+	
+	struct cmdline *l;
+	l = parsecmd( & line);
+	if (l->err) {
+		/* Syntax error, read another command */
+		printf("error: %s\n", l->err);
+		return 1;
+	}
+	pid_t process = create_processes(l->seq, l->in, l->out);
+	if(!l->bg){
+		waitpid(process, NULL, 0);
+	}
 
 	/* Remove this line when using parsecmd as it will free it */
-	free(line);
+	//free(line);
 	
 	return 0;
 }
@@ -67,10 +153,10 @@ int main() {
         scm_c_define_gsubr("executer", 1, 0, 0, executer_wrapper);
 #endif
 
+		pidTable = create_pid_list();
+
 	while (1) {
-		struct cmdline *l;
 		char *line=0;
-		int i, j;
 		char *prompt = "ensishell>";
 
 		/* Readline use some internal memory structure that
@@ -79,6 +165,14 @@ int main() {
 		line = readline(prompt);
 		if (line == 0 || ! strncmp(line,"exit", 4)) {
 			terminate(line);
+		}
+		else if(!strncmp(line, "jobs", 4)){
+			clean_pid_list(&pidTable);
+			print_pid_list(pidTable);
+#ifdef USE_GNU_READLINE
+		add_history(line);
+#endif
+			continue;
 		}
 
 #ifdef USE_GNU_READLINE
@@ -97,36 +191,39 @@ int main() {
                 }
 #endif
 
-		/* parsecmd free line and set it up to 0 */
-		l = parsecmd( & line);
-
-		/* If input stream closed, normal termination */
-		if (!l) {
-		  
-			terminate(0);
-		}
-		
-
-		
-		if (l->err) {
-			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
-		}
-
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
-
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
+		executer(line);
+//		struct cmdline *l;
+//		int i, j;
+//		/* parsecmd free line and set it up to 0 */
+//		l = parsecmd( & line);
+//
+//		/* If input stream closed, normal termination */
+//		if (!l) {
+//		  
+//			terminate(0);
+//		}
+//		
+//
+//		
+//		if (l->err) {
+//			/* Syntax error, read another command */
+//			printf("error: %s\n", l->err);
+//			continue;
+//		}
+//
+//		if (l->in) printf("in: %s\n", l->in);
+//		if (l->out) printf("out: %s\n", l->out);
+//		if (l->bg) printf("background (&)\n");
+//
+//		/* Display each command of the pipe */
+//		for (i=0; l->seq[i]!=0; i++) {
+//			char **cmd = l->seq[i];
+//			printf("seq[%d]: ", i);
+//                        for (j=0; cmd[j]!=0; j++) {
+//                                printf("'%s' ", cmd[j]);
+//                        }
+//			printf("\n");
+//		}
 	}
 
 }
